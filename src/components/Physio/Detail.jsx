a339e952f0a0d411f883c5bd78d37e945659d178
arslan456 <muhammadarslanjabbar@gmail.com>
2023-07-04 17:55:48 +0500

    'callback-added'

--- src/components/Physio/Detail.jsx
+++ src/components/Physio/Detail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import Banner from '../Common/Banner'
 import { FaCheckCircle, FaCaretDown } from "react-icons/fa";
 import Image from 'next/image'
